63503d0c38d27692f48d07a3252decd631cf9d75
LAPTOP-VKH4P47T\DASH F15 <ajunquit@gmail.com>
2023-05-22 08:45:50 -0500

    Add project files.

---
+++ design-pattern-builder/IBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_pattern_builder
+{
+    public interface IBuilder
+    {
+        public void BuildPartA();
+        public void BuildPartB();
+        public void BuildPartC();
+    }
+}
---
+++ design-pattern-builder/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using design_pattern_builder;
+
+Console.WriteLine("Hello, World!");
+
+var director = new Director();
+var builder = new ConcreteBuilder();
+director.Builder = builder;
+
+Console.WriteLine("Standard basic product:");
+director.BuildMinimalViableProduct();
+Console.WriteLine(builder.GetProduct().ListParts());
+
+Console.WriteLine("Standard full featured product:");
+director.BuildFullFeaturedProduct();
+Console.WriteLine(builder.GetProduct().ListParts());
+
+Console.WriteLine("Custom product:");
+builder.BuildPartA();
+builder.BuildPartC();
+Console.Write(builder.GetProduct().ListParts());
